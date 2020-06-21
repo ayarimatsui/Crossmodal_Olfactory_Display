70c5e693ce0c11d60651635f2973db313c6e9a29
ayarimatsui <matsui-ayari682@g.ecc.u-tokyo.ac.jp>
2020-06-21 17:27:48 +0900

    updated PID control script

--- Arduino/PID_control/PID_control.ino
+++ Arduino/PID_control/PID_control.ino
@@ -1,20 +1,63 @@
+//温度を上げたい時
 #include <PID_v1.h>
 
 //Define Variables we'll be connecting to
-double Setpoint, Input, Output;
+//PID制御関連の変数、初期設定
+double Setpoint, Input, Output; //PID制御の目標値、入力、出力
 
 //Define the aggressive and conservative Tuning Parameters
-double aggKp=4, aggKi=0.2, aggKd=1;
-double consKp=1, consKi=0.05, consKd=0.25;
+double aggKp=150, aggKi=10, aggKd=1;
+double consKp=50, consKi=0.1, consKd=0.8;
 
 //Specify the links and initial tuning parameters
 PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, DIRECT);
 
+
+//IC温度センサ、ペルチェ制御関連の変数とか
+const int sensorPin_1 = A0;
+const int sensorPin_2 = A1; 
+float sensorValue_1;
+float voltageOut_1;
+float sensorValue_2;
+float voltageOut_2;
+
+float temperatureC_1;
+float temperatureC_2;
+
+//double PE; //初期誤差
+
+const int Peltier_in1 = 0;  //Arduino デジタル信号入力
+const int Peltier_in2 = 1;  //Arduino デジタル信号入力
+const int PWM_output = 3;  //PWM制御 (アナログ入力)
+
 void setup()
 {
+  //温度センサからの入力を初期化
+  pinMode(sensorPin_1, INPUT);
+  pinMode(sensorPin_2, INPUT);
+  Serial.begin(9600);
+  sensorValue_1 = analogRead(sensorPin_1);
+  sensorValue_2 = analogRead(sensorPin_2);
+  voltageOut_1 = (sensorValue_1 * 5000) / 1024;
+  voltageOut_2 = (sensorValue_2 * 5000) / 1024;
+  // calculate temperature for LM335
+  temperatureC_1 = voltageOut_1 / 10 - 273;
+  temperatureC_2 = voltageOut_2 / 10 - 273;
+  //PE = (double)(temperatureC_2 - temperatureC_1); //1が室温、2が制御側
+
+  Serial.print("Initialized  Temperature_1(ºC): ");
+  Serial.print(temperatureC_1);
+  Serial.print("  Temperature_2(ºC): ");
+  Serial.println(temperatureC_2);
+
+  //ペルチェ制御の初期化
+  pinMode(Peltier_in1, OUTPUT);
+  pinMode(Peltier_in2, OUTPUT);
+
+  //PID制御の初期化
   //initialize the variables we're linked to
-  Input = analogRead(0);
-  Setpoint = 100;
+  Input = (double)(temperatureC_2 - temperatureC_1);
+  Setpoint = 2;
 
   //turn the PID on
   myPID.SetMode(AUTOMATIC);
@@ -22,11 +65,23 @@ void setup()
 
 void loop()
 {
-  Input = analogRead(0);
+  sensorValue_1 = analogRead(sensorPin_1);
+  sensorValue_2 = analogRead(sensorPin_2);
+  voltageOut_1 = (sensorValue_1 * 5000) / 1024;
+  voltageOut_2 = (sensorValue_2 * 5000) / 1024;
+  // calculate temperature for LM335
+  temperatureC_1 = voltageOut_1 / 10 - 273;
+  temperatureC_2 = voltageOut_2 / 10 - 273;
+  
+  Input = (double)(temperatureC_2 - temperatureC_1);
 
-  double gap = abs(Setpoint-Input); //distance away from setpoint
-  if(gap<10)
-  {  //we're close to setpoint, use conservative tuning parameters
+  double gap = abs(Setpoint - Input); //distance away from setpoint
+
+  digitalWrite(Peltier_in1, HIGH); //正
+  digitalWrite(Peltier_in2, LOW);
+  
+  if (gap < 0.3) {
+    //we're close to setpoint, use conservative tuning parameters
     myPID.SetTunings(consKp, consKi, consKd);
   }
   else
@@ -36,5 +91,14 @@ void loop()
   }
 
   myPID.Compute();
-  analogWrite(3,Output);
+  analogWrite(PWM_output, Output);
+  
+  Serial.print("Temperature_1(ºC): ");
+  Serial.print(temperatureC_1);
+  Serial.print("  Temperature_2(ºC): ");
+  Serial.print(temperatureC_2);
+  Serial.print("  Current Gap : ");
+  Serial.print(Setpoint - Input);
+  Serial.print("  Output(V) : ");
+  Serial.println(6 * Output / 255);
 }
