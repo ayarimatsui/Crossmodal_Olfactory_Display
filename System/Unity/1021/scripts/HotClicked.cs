4c9344cec248a77d2a2b2c9d3a232a7c8a877385
松井彩里 <matsui-ayari682@g.ecc.u-tokyo.ac.jp>
2020-10-22 01:29:19 +0900

    rearranged directories

---
+++ System/Unity/1021/scripts/HotClicked.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotClicked : MonoBehaviour {
+
+    public SerialHandler serialHandler;
+
+    // ボタンが押された場合、今回呼び出される関数
+    public void OnClick()
+    {
+        Debug.Log("Hot Button Clicked!");  // ログを出力
+        serialHandler.Write("H");
+    }
+}
---
+++ System/Unity/1021/scripts/StopClicked.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopClicked : MonoBehaviour
+{
+
+    public SerialHandler serialHandler;
+
+    // ボタンが押された場合、今回呼び出される関数
+    public void OnClick()
+    {
+        Debug.Log("Stop Button Clicked!");  // ログを出力
+        serialHandler.Write("0");
+    }
+}
+
