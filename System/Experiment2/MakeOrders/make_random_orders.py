36e423089d5aee1cfcedb9539ed2ade7d509cd92
松井彩里 <matsui-ayari682@g.ecc.u-tokyo.ac.jp>
2020-12-09 14:08:58 +0900

    added MakeOrders programs

---
+++ System/Experiment2/MakeOrders/make_random_orders.py
@@ -0,0 +1,15 @@
+# 実験2
+# グレープフルーツ、アップル、チェリー、マスカット、ラズベリー、さくらの6種類の香料をランダムに並び替えてcsvファイルに保存
+
+import random
+import csv
+
+N = 6 #被験者数
+
+for n in range(1, N + 1):
+    aroma_list = ['grapefruit', 'apple', 'cherry', 'white grapes', 'raspberry', 'sakura']
+    random.shuffle(aroma_list)
+    with open('csvFiles/order_subject' + str(n) + '.csv', 'w') as f:
+        writer = csv.writer(f)
+        for i in range(len(aroma_list)):
+            writer.writerow([i + 1, aroma_list[i]])
